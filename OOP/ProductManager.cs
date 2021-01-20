6e0a847aca1b25f4f0c80f99f726764989942d7a
PC <PC@DESKTOP-TDG19QO>
2021-01-20 23:52:48 +0300

    20 ocak

---
+++ OOP/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class ProductManager
+    {
+        //encapsulation
+        //product değeinde değer ver
+        public void Add(Product product)//101
+        {
+            Console.WriteLine(product.ProductName+"Eklendi");
+
+        }
+         public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi");
+        }
+
+        
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.UnitPrice = 500;
+            product1.UnitStock = 3;
+
+            Product product2 = new Product {Id=2,CategoryId=5,
+                UnitStock=5,ProductName="Kalem",UnitPrice= 35};
+
+            //sayısallar değer tip
+            //ref out keywordleri
+            //int double bool decimal...değer tip
+            //dizler class abstractclass interface referans tip
+            //PascalCase   //camelCase
+            //case senstıve küçük büyük harf önenmi isimlendirme
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+
+
+        }
+    }
+}
